ffee153d16facc75cb7ac11c1c2c48c39403ee09
Kordrad <kord.stp@gmail.com>
2020-07-07 16:47:29 +0200

    feat(style): add simple styles for application with material components

--- apps/todo/src/app/app.module.ts
+++ apps/todo/src/app/app.module.ts
@@ -6,12 +6,19 @@ import { FormsModule } from '@angular/forms';
 import { TodoDataAccessStoreModule } from '@todo-workspace/todo/data-access/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule, TodoDataAccessStoreModule, EffectsModule.forRoot([]), StoreDevtoolsModule.instrument({
     maxAge: 25
-  })],
+  }), BrowserAnimationsModule, MatSliderModule, MatInputModule, MatButtonModule, MatIconModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
