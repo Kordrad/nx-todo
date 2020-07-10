d4e7b6682241075ec1432caf089616ad03613f06
Kordrad <kord.stp@gmail.com>
2020-07-10 12:49:22 +0200

    feat(): remove unused files of styles, change detection on push app

--- libs/todo/shell/src/lib/containers/tasks/tasks.component.ts
+++ libs/todo/shell/src/lib/containers/tasks/tasks.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'todo-workspace-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TasksComponent {
