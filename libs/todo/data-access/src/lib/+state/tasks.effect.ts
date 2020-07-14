013ba44d20f87fec46efe2aadfe18999787ea778
Kordrad <kord.stp@gmail.com>
2020-07-14 11:55:24 +0200

    perf(tasks.effect): improve effect to use pessimisticUpdate

--- libs/todo/data-access/src/lib/+state/tasks.effect.ts
+++ libs/todo/data-access/src/lib/+state/tasks.effect.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { TaskDataService } from '../services/task-data.service';
 import { tasksActions } from './tasks.actions';
-import { concatMap, map } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
-import { TaskState } from './tasks.reducer';
-import { fetch, optimisticUpdate, pessimisticUpdate } from '@nrwl/angular';
+import { map } from 'rxjs/operators';
+import { fetch, pessimisticUpdate } from '@nrwl/angular';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class TasksEffect {
@@ -24,41 +23,57 @@ export class TasksEffect {
     })
   );
 
-  @Effect({ dispatch: false })
+  @Effect()
   createTask$ = this.actions$.pipe(
     ofType(tasksActions.Types.CreateTask),
-    concatMap((action: tasksActions.Create) => {
-      if (action.payload._start && action.payload._limit) {
-        const { _start, _limit } = action.payload;
-        this.store$.dispatch(new tasksActions.Load({ _start, _limit }));
-      }
-      return this.taskDataService.createTask(action.payload.task);
+    pessimisticUpdate({
+      run: (action: tasksActions.Create) => {
+        const { task, _limit, _start } = action.payload;
+        return this.taskDataService.createTask(task).pipe(
+          map((data) => {
+            return new tasksActions.Load({ _limit, _start });
+          })
+        );
+      },
+      onError: (action: tasksActions.Create, error: HttpErrorResponse) => {
+        return error;
+      },
     })
   );
 
-  @Effect({ dispatch: false })
+  @Effect()
   deleteTask$ = this.actions$.pipe(
     ofType(tasksActions.Types.DeleteTask),
-    concatMap((action: tasksActions.Delete) => {
-      const { _start, _limit } = action['payload'];
-      if (_start && _limit) {
-        this.store$.dispatch(new tasksActions.Load({ _start, _limit }));
-      }
-      return this.taskDataService.deleteTask(action.payload.id);
+    pessimisticUpdate({
+      run: (action: tasksActions.Delete) => {
+        const { id, _limit, _start } = action.payload;
+        return this.taskDataService.deleteTask(id).pipe(
+          map((data) => {
+            return new tasksActions.Load({ _limit, _start });
+          })
+        );
+      },
+      onError: (action: tasksActions.Delete, error: HttpErrorResponse) => {
+        return error;
+      },
     })
   );
 
   @Effect({ dispatch: false })
   updateTask$ = this.actions$.pipe(
     ofType(tasksActions.Types.UpdateTask),
-    concatMap((action: tasksActions.Update) => {
-      return this.taskDataService.updateTask(action.task);
+    pessimisticUpdate({
+      run: ({ task }: tasksActions.Update) => {
+        return this.taskDataService.updateTask(task)
+      },
+      onError: (action: tasksActions.Update, error: HttpErrorResponse) => {
+        return error;
+      },
     })
   );
 
   constructor(
     private taskDataService: TaskDataService,
-    private actions$: Actions,
-    private store$: Store<TaskState>
+    private actions$: Actions
   ) {}
 }
