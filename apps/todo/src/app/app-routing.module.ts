5eef546d317b16c8e907769e08d5877fd7073a46
Kordrad <kord.stp@gmail.com>
2020-07-15 10:16:34 +0200

    fix(router): resolve problem with change page via pagination
    Router option block change link via pagination

--- apps/todo/src/app/app-routing.module.ts
+++ apps/todo/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'page/1', pathMatch: 'full' },
-  { path: '**', redirectTo: 'page/1', pathMatch: 'full' },
-  ];
+const routes: Routes = [{ path: '', redirectTo: 'page/1', pathMatch: 'full' }];
 
 @NgModule({
   declarations: [],
