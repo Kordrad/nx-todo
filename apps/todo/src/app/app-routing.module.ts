4e9711121837471c1db40da8ea343bbd068976f9
Kordrad <kord.stp@gmail.com>
2020-07-15 10:05:43 +0200

    feat(router): change link to page/1 if you open wrong link

--- apps/todo/src/app/app-routing.module.ts
+++ apps/todo/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', redirectTo: 'page/1', pathMatch: 'full' }];
+const routes: Routes = [
+  { path: '', redirectTo: 'page/1', pathMatch: 'full' },
+  { path: '**', redirectTo: 'page/1', pathMatch: 'full' },
+  ];
 
 @NgModule({
   declarations: [],
