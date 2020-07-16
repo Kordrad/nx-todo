2fedca4308a5be71cc52cfc0d31db84e3888f534
Kordrad <kord.stp@gmail.com>
2020-07-16 08:29:58 +0200

    ci(ToDoAccessModule): transfer import module to todo-feature.module

--- apps/todo/src/app/app.module.ts
+++ apps/todo/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
-import { TodoDataAccessModule } from '@todo-workspace/todo/data-access';
 import { TodoShellModule } from '@todo-workspace/todo/shell';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
@@ -13,7 +12,6 @@ import { apiUrl } from '@todo-workspace/todo/domain';
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
-    TodoDataAccessModule,
     TodoShellModule,
     StoreModule.forRoot([]),
     EffectsModule.forRoot([]),
--- libs/todo/feature/src/lib/todo-feature.module.ts
+++ libs/todo/feature/src/lib/todo-feature.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodoUiTaskFormModule } from '@todo-workspace/todo/ui-task-form';
 import { TodoUiTaskListModule } from '@todo-workspace/todo/ui-task-list';
+import { TodoDataAccessModule } from '@todo-workspace/todo/data-access';
 
 @NgModule({
-  imports: [CommonModule, TodoUiTaskFormModule, TodoUiTaskListModule],
+  imports: [
+    CommonModule,
+    TodoDataAccessModule,
+    TodoUiTaskFormModule,
+    TodoUiTaskListModule,
+  ],
   declarations: [TodoComponent],
   exports: [TodoComponent],
   entryComponents: [TodoComponent],
