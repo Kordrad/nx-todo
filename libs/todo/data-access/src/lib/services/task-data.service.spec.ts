276656995b7b134790898c64987d9a4d31fc86bc
Kordrad <kord.stp@gmail.com>
2020-07-17 14:21:30 +0200

    tests(task-data.service): add more tests for testing service functions

--- libs/todo/data-access/src/lib/services/task-data.service.spec.ts
+++ libs/todo/data-access/src/lib/services/task-data.service.spec.ts
@@ -5,46 +5,17 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TaskDataService } from './task-data.service';
+import { first } from 'rxjs/operators';
 
-const mockTasks = [
-  {
-    userId: 1,
-    id: 1,
-    title: 'delectus aut autemXXXD',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 2,
-    title: 'quis ut nam facilis et officia qui',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 3,
-    title: 'fugiat veniam minus',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 4,
-    title: 'et porro tempora',
-    completed: true,
-  },
-  {
-    userId: 1,
-    id: 5,
-    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
-    completed: false,
-  },
-];
 const optionsService = {
   _start: '1',
   _limit: '5',
 };
 const url = 'https://test/';
+const apiUrl = `${url}todos/`;
+
 describe('TaskDataService', () => {
-  let myProvider: TaskDataService;
+  let service: TaskDataService;
   let httpMock: HttpTestingController;
 
   beforeEach(async () => {
@@ -62,22 +33,107 @@ describe('TaskDataService', () => {
 
   beforeEach(() => {
     httpMock = TestBed.inject(HttpTestingController);
-    myProvider = TestBed.inject(TaskDataService);
+    service = TestBed.inject(TaskDataService);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should return an Observable<Task[]>', () => {
-    myProvider.getAllTasks(optionsService).subscribe((tasks: Task[]) => {
-      expect(tasks.length).toBe(5);
+    const dummyTasks = [
+      {
+        userId: 1,
+        id: 1,
+        title: 'delectus aut autemXXXD',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: 'quis ut nam facilis et officia qui',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: 'fugiat veniam minus',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: 'et porro tempora',
+        completed: true,
+      },
+      {
+        userId: 1,
+        id: 5,
+        title:
+          'laboriosam mollitia et enim quasi adipisci quia provident illum',
+        completed: false,
+      },
+    ];
+    let arr = [];
+    service.getAllTasks(optionsService).subscribe((tasks: Task[]) => {
+      arr = [...tasks];
     });
 
-    const request = httpMock.expectOne(
-      url + `todos/?_start=${optionsService._start}&_limit=${optionsService._limit}`
+    const req = httpMock.expectOne(
+      apiUrl +
+        `?_start=${optionsService._start}&_limit=${optionsService._limit}`
     );
 
-    expect(request.request.method).toBe('GET');
+    expect(req.request.method).toBe('GET');
 
-    request.flush(mockTasks);
+    req.flush(dummyTasks);
+    expect(arr).toHaveLength(5);
+    expect(arr).toEqual(dummyTasks);
+  });
 
-    httpMock.verify();
+  it('should delete task', () => {
+    const dummyTasks = [
+      {
+        userId: 1,
+        id: 1,
+        title: 'delectus aut autemXXXD',
+        completed: false,
+      },
+    ];
+    service.deleteTask(1).pipe(first()).subscribe();
+    const req = httpMock.expectOne(apiUrl + '1');
+    expect(req.request.method).toBe('DELETE');
+    req.flush(dummyTasks);
+  });
+
+  it('should create task', () => {
+    const dummyTask = {
+      userId: 1,
+      id: 6,
+      title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+      completed: false,
+    };
+
+    service.createTask(dummyTask).pipe(first()).subscribe();
+    const req = httpMock.expectOne(apiUrl);
+    expect(req.request.method).toBe('POST');
+    req.flush(dummyTask);
+  });
+
+  it('should update task', () => {
+    const dummyTask = {
+      userId: 1,
+      id: 3,
+      title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+      completed: false,
+    };
+
+    service
+      .updateTask({ ...dummyTask, completed: true })
+      .pipe(first())
+      .subscribe();
+    const req = httpMock.expectOne(apiUrl + dummyTask.id);
+    expect(req.request.method).toBe('PUT');
+    req.flush([dummyTask]);
   });
 });
